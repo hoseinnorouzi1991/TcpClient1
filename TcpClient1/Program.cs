522b90ca1d4e10edcb0d5bf5d5a910bb0757c689
Arash <mh.norouzi89@gmail.com>
2020-05-31 16:12:06 +0430

    first

---
+++ TcpClient1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpClient1
+{
+    class Program
+    {
+        private const int portNum = 13;
+        private const string hostName = "localhost";
+
+        public static int Main(String[] args)
+        {
+            try
+            {
+                var client = new TcpClient(hostName, portNum);
+
+                NetworkStream ns = client.GetStream();
+
+                byte[] bytes = new byte[1024];
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+
+                Console.WriteLine(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+
+                client.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
